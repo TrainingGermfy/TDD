916208b30544cff86bb74299ac83eb4e45ca148f
samuelrojolopez <71668376+samuelrojolopez@users.noreply.github.com>
2020-10-09 17:44:32 -0500

    New tests for substraction multiply and divide

--- tests/test_calcudora.py
+++ tests/test_calcudora.py
@@ -14,3 +14,19 @@ class Test_MiCalculadora(unittest.TestCase):
     def test_metodo_suma(self):
         self.calc.add(1, 3)
         self.assertEqual(4, self.calc.valor)
+
+    def test_metodo_resta(self):
+        self.calc.substract(10, 5)
+        self.assertEqual(5, self.calc.valor)
+        
+    def test_metodo_multiplicacion(self):
+        self.calc.multiply(6, 3)
+        self.assertEqual(18, self.calc.valor)
+        
+    def test_metodo_division(self):
+        self.calc.divide(15, 3)
+        self.assertEqual(5, self.calc.valor)
+   
+    def test_metodo_divisioncero(self):
+        self.calc.divide(15, 0)
+        self.assertEqual(NULL, self.calc.valor)
