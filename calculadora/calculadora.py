bff6cde816b947f6f40ce9856de96b1748c156c8
Rodrigo Soto <rodrigo.soto@wizeline.com>
2020-10-12 12:29:55 -0500

    Added mult, div and resta operations. Added test cases for all operations

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -1,6 +1,29 @@
 class Calculadora:
     def __init__(self):
-        self.valor = 0        
+        self.valor = 0
 
-    def add(self, x, y):
-        self.valor = x + y
+    def add(self, x=0, y=0):
+        try:
+            self.valor = float(x) + float(y)
+        except ValueError as e:
+            self.valor = e
+
+    def div(self, numerador=0, denominador=1):
+        try:
+            self.valor = float(numerador) / float(denominador)
+        except ValueError as e:
+            self.valor = e
+        except ZeroDivisionError as e:
+            self.valor = e
+
+    def mult(self, x=0, y=0):
+        try:
+            self.valor = float(x) * float(y)
+        except ValueError as e:
+            self.valor = e
+
+    def resta(self, x=0, y=0):
+        try:
+            self.valor = float(x) - float(y)
+        except ValueError as e:
+            self.valor = e
--- tests/test_calcudora.py
+++ tests/test_calcudora.py
@@ -1,16 +1,79 @@
 import unittest
-
 from calculadora.calculadora import Calculadora
 
 
-class Test_MiCalculadora(unittest.TestCase):
+class TestMiCalculadora(unittest.TestCase):
 
     def setUp(self):
         self.calc = Calculadora()
-    
+
     def test_inicial_valores(self):
         self.assertEqual(0, self.calc.valor)
 
-    def test_metodo_suma(self):
+    def test_metodo_division_entre_cero(self):
+        self.calc.div(0, 0)
+        self.assertIsInstance(self.calc.valor, ZeroDivisionError)
+
+    def test_metodo_division_enteros(self):
+        self.calc.div(1, 3)
+        self.assertEqual(1 / 3, self.calc.valor)
+
+    def test_metodo_division_floats(self):
+        self.calc.div(10, 2.5)
+        self.assertEqual(4.0, self.calc.valor)
+
+    def test_metodo_division_strings_validos(self):
+        self.calc.div('1', '3')
+        self.assertEqual(1 / 3, self.calc.valor)
+
+    def test_metodo_division_strings_invalidos(self):
+        self.calc.div('2', 'a')
+        self.assertIsInstance(self.calc.valor, ValueError)
+
+    def test_metodo_mult_enteros(self):
+        self.calc.mult(1, 3)
+        self.assertEqual(3, self.calc.valor)
+
+    def test_metodo_mult_floats(self):
+        self.calc.mult(1.0, 3.0)
+        self.assertEqual(3.0, self.calc.valor)
+
+    def test_metodo_mult_strings_validos(self):
+        self.calc.mult('1', '3')
+        self.assertEqual(3, self.calc.valor)
+
+    def test_metodo_mult_string_invalidos(self):
+        self.calc.mult('a', '1')
+        self.assertIsInstance(self.calc.valor, ValueError)
+
+    def test_metodo_resta_enteros(self):
+        self.calc.resta(3, 1)
+        self.assertEqual(2, self.calc.valor)
+
+    def test_metodo_resta_floats(self):
+        self.calc.resta(3.0, 1.0)
+        self.assertEqual(2.0, self.calc.valor)
+
+    def test_metodo_resta_strings_validos(self):
+        self.calc.resta('1', '3')
+        self.assertEqual(-2, self.calc.valor)
+
+    def test_metodo_resta_string_invalidos(self):
+        self.calc.add('a', '1')
+        self.assertIsInstance(self.calc.valor, ValueError)
+
+    def test_metodo_suma_enteros(self):
         self.calc.add(1, 3)
         self.assertEqual(4, self.calc.valor)
+
+    def test_metodo_suma_floats(self):
+        self.calc.add(1.0, 3.0)
+        self.assertEqual(4.0, self.calc.valor)
+
+    def test_metodo_suma_strings_validos(self):
+        self.calc.add('1', '3')
+        self.assertEqual(4, self.calc.valor)
+
+    def test_metodo_suma_string_invalidos(self):
+        self.calc.add('a', '1')
+        self.assertIsInstance(self.calc.valor, ValueError)
