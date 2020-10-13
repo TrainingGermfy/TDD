aad83fc10d4b212ad025b82c9647d2e3cc2cd2ed
Cesar Ivan Piña Martinez <cesar.pina@cesarpina.local>
2020-10-13 10:06:17 -0500

    add division method and tests

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -1,6 +1,17 @@
 class Calculadora:
+
     def __init__(self):
-        self.valor = 0        
+        self.valor = 0
 
     def add(self, x, y):
         self.valor = x + y
+
+    def divide_int(self, x, y):
+        if y == 0:
+            raise ZeroDivisionError
+        return x//y
+
+    def divide(self, x, y):
+        if y == 0:
+            raise ZeroDivisionError
+        return x/y
