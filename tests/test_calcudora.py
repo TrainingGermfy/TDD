c94819f7c6d58c0c1371a7c562e2b6cdca8e7dee
David V. Martínez <48252203+davidvemtz@users.noreply.github.com>
2020-10-13 14:29:46 -0500

    More tests were added too

--- tests/test_calcudora.py
+++ tests/test_calcudora.py
@@ -14,3 +14,15 @@ class Test_MiCalculadora(unittest.TestCase):
     def test_metodo_suma(self):
         self.calc.add(1, 3)
         self.assertEqual(4, self.calc.valor)
+    
+    def test_metodo_resta(self):
+        self.calc.substract(7, 5)
+        self.assertEqual(2, self.calc.valor)
+
+    def test_metodo_division(self):
+        self.calc.divide(13, 2)
+        self.assertEqual(6.5, self.calc.valor)
+
+    def test_metodo_multiplicacion(self):
+        self.calc.multiplication(9, 4)
+        self.assertEqual(36, self.calc.valor)
